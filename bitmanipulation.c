ad7404b9af36eaef8db0ef127061ea7637385bd2
joao cesar <joaocesarcriscolo@gmail.com>
2021-10-20 20:41:45 -0300

    abort error

--- bitmanipulation.c
+++ bitmanipulation.c
@@ -1,60 +1,75 @@
 // Bit manipulation baseado em
 // https://www.mathcs.emory.edu/%7Echeung/Courses/255/Syllabus/1-C-intro/bit-array.html
-#include "bitops.h"
 #include <stdint.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
 
+void bitManipulation();
+int main() {
+  bitManipulation();
+  return 0;
+}
 void bitManipulation() {
-/* 
-int = 32bits/4bytes
-*/
-
 // Metadata 8bytes 
 // 2 bytes cada entrada
 
-  uint16_t metaIndice = 256
-
+  uint8_t metaIndice = 255;
+  
   // ○ Tamanho do índice (quantas entradas o índice possui, utilizar o valor 2^8)
   // indice 0 - 15
-  // 0000 0001 0000 0000
 
   // ○ Tamanho do cluster (utilizar o valor 32KB)
   // 16 - 31
-  uint8_t metaTamanhoCuster = 32
+  uint8_t metaClusterSize = 32;
 
-  // ○ Byte onde o índice inicia (metadados iniciam no byte zero e vão até byte 7)
+  // ○ Byte onde o índice inicia (metadados iniciam no byte zero e vão até byte 3)
   // 0000 0111
   // 32 - 47
-  uint8_t metaTamanhoCuster = 24
+  uint8_t fatBegin = 4;
 
   // ○ Byte onde inicia o primeiro cluster
   // 8bytes metadata + 256bytes indice =  byte 264 
   // 100001000
   // 48- 63
+  uint16_t clusterBegin = 259;
 
-/* 
-   Indice =  entradas de 8 bits
-   2^8 = 256 entradas, cada entrada 1 byte
-   256/4(4bytes 1 int) = 64 ints
-  */
-  unsigned int indice[63];
-  for (int i = 0; i < 64; i++)
-    indice[i] = 0;                    // Clear the bit array
-
-// ○ ponteiro
-// ○ EOF
-// ○ vazio
-// ○ corrompido
+  unsigned int indice[254];
+  for (int i = 0; i < 255; i++)
+    indice[i] = 255;                    // Clear the bit array
 
   /*
     Os clusters são posicionados logo abaixo do índice e possuem 32KB de tamanho cada.
     A estrutura interna dos clusters é de livre escolha do grupo
     32KB = 256000bits = 8000ints cada cluster
     256 cluster totais
-  */
-  // dependerá do espaco que deixarmos para a arvore
+   dependerá do espaco que deixarmos para a arvore
+  
+ 
   unsigned int clusters[255][8000];
-  for (i = 0; i < 256; i++) {
+  for (int i = 0; i < 256; i++) {
     for(int j = 0; j<8000; j++) 
       clusters[i][j] = 0;
   }
+
+	FILE* out = fopen("lighfs.bin", "wb");
+   if (NULL==out) {
+       fprintf(stderr, "I bet you saw THAT coming.\n");
+       exit(EXIT_FAILURE);
+   }
+
+   fwrite(&metaIndice, 1,1, out);
+   fwrite(&metaClusterSize, 1,1, out);
+   fwrite(&fatBegin, 1,1, out);
+   fwrite(&clusterBegin, 2,1, out);
+	int num = 255;
+   fwrite(&num, 255,1, out);
+	num = 0;
+   fwrite(&num, 8160000,1, out);
+   if(ferror(out)){
+      perror(__func__);
+      exit(EXIT_FAILURE);
+   }
+   printf("asdsadas");
+   */
+}
 
